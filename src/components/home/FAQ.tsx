1d01f46114fd366388f584369d8be21e650fda0b
shathipaul <shathipaul11@gmail.com>
2025-01-17 22:57:34 +0600

    added faq section

---
+++ src/components/home/FAQ.tsx
@@ -0,0 +1,87 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Image from "next/image";
+import germyImg from "@/assets/germy/Germy Wormy.png";
+import OnScrollAnimation from "../animation/OnScrollAnimation";
+
+const FAQ = () => {
+  const faqData = [
+    {
+      ques: "What is Germy Wormy?",
+      ans: "Germy Wormy is a fun and humorous meme token built on the Solana blockchain. It’s designed to bring joy to the crypto space while fostering a strong, engaged community.",
+    },
+    {
+      ques: "How can I buy Germy Wormy?",
+      ans: 'You can purchase Germy Wormy on Raydium by swapping SOL for GERM. Check out the "How to Buy" section for a step-by-step guide.',
+    },
+    {
+      ques: "What is the total supply of Germy Wormy?",
+      ans: "Germy Wormy has a total supply of 23,000,000 tokens.",
+    },
+    {
+      ques: "What is the token symbol?",
+      ans: "The token symbol is GERM.",
+    },
+    {
+      ques: "What makes Germy Wormy unique?",
+      ans: "Germy Wormy combines meme culture with a vibrant community and future plans for staking, NFTs, and games. It’s about having fun while wiggling to the moon!",
+    },
+    {
+      ques: "Where can I find updates and connect with the community?",
+      ans: "Follow us on Instagram, TikTok, Twitter, and Facebook, and join our Discord for the latest updates, memes, and giveaways.",
+    },
+  ];
+  return (
+    <OnScrollAnimation>
+      <div className="bg-white w-full relative z-20 py-10">
+        <div className="container customLeftRightPadding">
+          <h3 className="titleFont capitalize text-center text-3xl md:text-4xl font-semibold tracking-wider mb-4 md:mb-10">
+            Frequently asked questions
+          </h3>
+          <div className="grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-4">
+            <div className="w-[80%] hidden lg:block">
+              <Image src={germyImg} alt="" />
+            </div>
+            <div className="flex flex-col gap-4">
+              {faqData.map((data, index) => (
+                <Accordion key={index} defaultExpanded={index === 0}>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                  >
+                    <Typography
+                      style={{
+                        fontFamily: "Comic Neue, sans-serif",
+                        fontWeight: 600,
+                      }}
+                    >
+                      {data.ques}
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography
+                      style={{
+                        fontFamily: "Nunito, sans-serif",
+                        fontWeight: 400,
+                      }}
+                    >
+                      {data.ans}
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </OnScrollAnimation>
+  );
+};
+
+export default FAQ;
--- src/components/home/HomeMain.tsx
+++ src/components/home/HomeMain.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import FAQ from "./FAQ";
 
 const HomeMain = () => {
   return (
     <>
       <h2>Home</h2>
+
+      <div className="bg-white h-[1000px] py-20"></div>
+      <FAQ />
     </>
   );
 };
